f1ddff0f13db45ce841caaf656f7070d2f01e494
Carlos Andres Ramirez Muñiz <munizc@CAMPUSUNINORTE.LOCAL>
2019-04-05 13:54:58 -0500

    ed1

--- Floyd Warshall/src/floyd/warshall/FloydWarshall.java
+++ Floyd Warshall/src/floyd/warshall/FloydWarshall.java
@@ -15,11 +15,13 @@ public class FloydWarshall {
      * @param args the command line arguments
      */
     
-    static void floyd_warshall(int grafo [][]){
+    static void floyd_warshall(int grafo[][]){
     
         int distancia[][]=getMatrix(0,grafo);
         int caminos[][]=getMatrix(1,grafo);
         int tmp;
+        print("Matiz de distancia inicial: ",distancia);
+        print("Matiz de caminos inicial: ",caminos);
         for (int k = 0; k < 10; k++) {
             for (int i = 0; i < 10; i++) {
                 for (int j = 0; j < 10; j++) {
@@ -31,7 +33,27 @@ public class FloydWarshall {
                 }
             }
         }
+        print("Matiz de distancia final: ",distancia);
+        print("Matiz de caminos final: ",caminos);
     }
+    
+    static void getMatrix(int grafo[][]){
+    
+    }
+    
+    static void print(String title, int M[][]) {
+        System.out.println(title);
+        int n = M.length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) 
+                System.out.print(M[i][j] + "   ");
+            
+
+            System.out.println();
+        }
+    }
+
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
